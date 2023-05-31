a41cf449041abae38f16f55b2072d2b25c0d333e
henriquelelli <henriquelelli@hotmail.com>
2023-05-31 01:46:11 +0000

    deployed

--- database.php
+++ database.php
@@ -1,10 +1,10 @@
 <?php
 
 class Database {
-    private $servername = "localhost:3306";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "gerenciamentoEventos";
+    private $servername = "127.0.0.1:3306";
+    private $username = "u822896253_henriquelelli";
+    private $password = "Hcl@330469";
+    private $dbname = "u822896253_skedway";
     private $connection;
 
     public function __construct() {
